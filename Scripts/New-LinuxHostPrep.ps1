983cd6a5b0e0995c167e31db9fb7ac76a3e57326
JayAreP <43276115+JayAreP@users.noreply.github.com>
2020-06-05 09:44:06 -0700

    Create New-LinuxHostPrep.ps1

---
+++ Scripts/New-LinuxHostPrep.ps1
@@ -0,0 +1,31 @@
+# Example Script for automated deployment of volumes using the SDP module. 
+
+param(
+    [parameter(mandatory)]
+    [string] $name,
+    [parameter(mandatory)]
+    [int] $sizeInGB,
+    [parameter(mandatory)]
+    [int] $numberOfVolumes
+)
+
+# Create the host
+Write-Host "Creating Host object -> $name"
+New-SDPHost -name $name -type Linux
+
+# Create the volume group
+$vgname = $name + '-vg'
+Write-Host "Creating Volume Group object -> $vgname"
+New-SDPVolumeGroup -name $vgname
+
+# Create the volumes
+$number = 1
+while ($number -le $numberOfVolumes) {
+    $volname = $name + '-vol-' + $number
+    Write-Host "Creating Volume object -> $volname"
+    New-SDPVolume -VolumeGroupName $vgname -sizeInGB $sizeInGB -name $volname
+    Write-Host "Mapping to host -> $name"
+    New-SDPMapping -volumeName $volname -hostName $name
+    $number++
+}
+
